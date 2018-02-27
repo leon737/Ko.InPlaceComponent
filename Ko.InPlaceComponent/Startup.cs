8076a876bf0d9aa4fe23657041723ea3ac3dd4a6
Leonid Gordo <leonardpt@gmail.com>
2018-02-27 23:31:20 +0300

    prepare bower package

--- Ko.InPlaceComponent/Startup.cs
+++ Ko.InPlaceComponent/Startup.cs
@@ -23,7 +23,6 @@ namespace Ko.InPlaceComponent
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,7 +33,6 @@ namespace Ko.InPlaceComponent
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
             app.UseDefaultFiles();
             app.UseStaticFiles();
 
